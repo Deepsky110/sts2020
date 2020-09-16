72b798dcde040493a0d8fe87a055b65c6be3cce3
Deepsky110 <58853370+Deepsky110@users.noreply.github.com>
2020-09-16 23:27:58 +0900

    Spring 3.x - ibatis&mybatis

---
+++ sts04/src/test/resources/com/bit/sts04/model/EmpDaoTest.java
@@ -0,0 +1,42 @@
+package com.bit.sts04.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class EmpDaoTest {
+	@Autowired
+	EmpDao empDao;
+
+	@Test
+	public void testSelectAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteOne() {
+		fail("Not yet implemented");
+	}
+
+}
